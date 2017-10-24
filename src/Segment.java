5ebce7b4ab4bc187ed7cde2189299c51e6134b0c
matt <matt@Chipouille>
2017-10-24 17:46:51 +0200

    - Ajout du triangle et du segment

---
+++ src/Segment.java
@@ -0,0 +1,10 @@
+
+public class Segment {
+	private Point p1;
+	private Point p2;
+	
+	public Segment(Point p1, Point p2) {
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+}
---
+++ src/Triangle.java
@@ -0,0 +1,10 @@
+
+public class Triangle {
+	private Segment seg1;
+	private Segment seg2;
+	private Segment seg3;
+	
+	public Triangle(Segment s1, Segment s2, Segment s3) {
+		
+	}
+}
