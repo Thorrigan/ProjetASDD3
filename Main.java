ff57f27064a5c2c731c664f3005ebd715533ad17
Thorrigan <33063975+Thorrigan@users.noreply.github.com>
2017-11-14 16:52:41 +0100

    szs

---
+++ Main.java
@@ -0,0 +1,58 @@
+package main;
+import Maths.Point;
+import Maths.Polygone;
+import Tests.TestsUnitairesMaths;
+
+/**
+ * Compléxité: Dans ce programme, nous noterons: 
+ * 1 : une compléxité constante
+ * n : le nombre d'itération dans le cas général
+ * log(n) : une compléxité logarithmique 
+ * nbp : le nombre de points 
+ * nbd : le nombre de droites
+ * nbs : le nombre de segments
+ * nbt : le nombre de triangles
+ * nbpg : le nombre de polygones
+ * @author matt
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		String nomFichier = "map.txt";
+		if(args.length != 0) {
+			nomFichier = args[0];
+		}
+		Point p1 = new Point(1.0f, 1.0f);
+		Point p2 = new Point(1.0f, 2.0f);
+		float angle = 90f;
+		System.out.println(angle);
+		Point p3 = p1.rotation(1f, angle);
+		System.out.println(p3);
+		//TestsUnitairesMaths.menuprincipalMaths();
+		
+		
+		/*Polygone pg = new Polygone();
+		pg.ajouterPoint(new Point(1.0f,1.0f));
+		pg.ajouterPoint(new Point(1.0f,3.0f));
+		pg.ajouterPoint(new Point(3.0f,3.0f));
+		pg.ajouterPoint(new Point(4.0f,4.0f));
+		pg.ajouterPoint(new Point(5.0f,4.0f));
+		pg.ajouterPoint(new Point(5.0f,2.0f));
+		pg.ajouterPoint(new Point(6.0f,1.0f));
+		pg.ajouterPoint(new Point(5.0f,0.0f));
+		pg.ajouterPoint(new Point(2.0f,1.0f));
+		
+		System.out.println(pg.contient(new Point(3.0f, 2.0f))); */
+		
+		/*
+		Triangle t = new Triangle(new Point(1.0f,1.0f), new Point(2.0f,1.0f), new Point(2.0f,2.0f));
+		
+		System.out.println(pg.PointsIntersection(t));
+		ArrayList<Triangle> tt = pg.triangulation();
+		for(Triangle t : tt) {
+			System.out.println(t);
+		}
+		
+		Jeu jeu = new Jeu(nomFichier);*/
+	}
+}
